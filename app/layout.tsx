118e4a377b0f77ba7960404f828a4a398f208c46
alex <alexxissmiki@gmail.com>
2026-09-21 12:41:13 -0500

    emailll fixed logoss change phone numberrRrsaa whats up number changeddss

--- app/layout.tsx
+++ app/layout.tsx
@@ -173,24 +173,33 @@ export default function RootLayout({
     >
       <head>
         <link rel="icon" href="/fetan-favicon.png" />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PQ7J9NJ5');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
         />
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E5VK4660N9" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-E5VK4660N9');
-            `,
-          }}
-        />
       </head>
       <body className="min-h-full bg-[#040e1a] font-sans text-white">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PQ7J9NJ5"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <VideoLoadingProvider>
           <LoadingScreen />
           <Navbar />
